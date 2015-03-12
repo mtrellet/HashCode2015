34132044d8e691c5fdbe2649058e8835db14f2c4
Xavier Martinez <xavier.martinez@u-psud.fr>
2015-03-12 19:31:50 +0100

    Fix le parsing pour indispos

--- parsing.py
+++ parsing.py
@@ -1,5 +1,5 @@
 import sys
-
+import random
 
 if len(sys.argv) != 2:
 	print sys.argv[0]," inputfile.in"
@@ -17,11 +17,31 @@ nbgroupes = int(tmp[3])
 nbservers = int(tmp[4])
 
 indispos = []
-for i in range(1,nbindispo):
+for i in range(1,nbindispo+1):
 	indispos.append([int(lines[i].split()[0]),int(lines[i].split()[1])])
 
 serveurs = []
 
+
+mysomme = 0
 for i in range(nbindispo+1,nbindispo+1+nbservers):
 	serveurs.append([int(lines[i].split()[0]),int(lines[i].split()[1])])
+	mysomme += int(lines[i].split()[0])
+
+matrice = []
+for i in range(nbranges):
+	matrice.append([])
+	for j in range(nbempla):
+		matrice[i].append([True])
+
+
+for i in range(nbindispo):
+	serv = indispos[i]
+	matrice[serv[0]][serv[1]] = False
+
+print matrice
+
+
+
+
 
